8a758c7d6b576cfee8af5e221f02c412c1cc1eca
EduDevHe <edu.he.dev@gmail.com>
2024-05-14 11:55:11 -0300

    feat: int and float

--- main.go
+++ main.go
@@ -3,4 +3,16 @@ import "fmt"
 
 func main(){
   fmt.Println("Hello World!")
+
+  var intNum int32 = 100
+  intNum = intNum + 1
+  fmt.Println(intNum)
+
+
+  var floatNum float32 = 100.1
+  floatNum = floatNum + 1
+  fmt.Println(intNum)
+
+  
+
 }
